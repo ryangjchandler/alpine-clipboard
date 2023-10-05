f5c4e22a4a69962d231f039d801f37cd8ae7c8a3
Ryan Chandler <ryangjchandler@gmail.com>
2023-10-05 15:51:34 +0100

    feature: add support for custom mime types

--- dist/alpine-clipboard.js
+++ dist/alpine-clipboard.js
@@ -5,13 +5,21 @@
 
     let onCopy = () => {};
 
-    const copy = target => {
+    const copy = (target, mimeType = undefined) => {
       if (typeof target === 'function') {
         target = target();
       }
 
       if (typeof target === 'object') {
         target = JSON.stringify(target);
+      }
+
+      if (mimeType !== undefined) {
+        return window.navigator.clipboard.write([new ClipboardItem({
+          [mimeType]: new Blob([target], {
+            type: mimeType
+          })
+        })]).then(onCopy);
       }
 
       return window.navigator.clipboard.writeText(target).then(onCopy);
